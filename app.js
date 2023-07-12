fd364e3ac4dc6da9c1311f1fdd6a486815bfa0a0
Jonish-sm <jonishsubba@gmail.com>
2023-07-12 10:41:50 +0530

    function

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 console.log("Hello");
 
-const x =[1,8,5,7,]
+const x = [1, 8, 5, 7];
 
-console.log(x)
+console.log(x);
 
-const [todos, setTodos] = useState([])
+const [todos, setTodos] = useState([]);
 
 const y = 12;
+
+function x(a, b) {
+  return b * a;
+}
+
+console.log(x(5, 7));
